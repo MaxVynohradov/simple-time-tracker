133c65f3fab0eb62257a1bedd2f567c43e78e0c3
Maksym Vynohradov <mv@madappgang.com>
2020-04-03 11:46:10 +0300

    fix(tasks): update tasks generator

--- src/utils/generateTasks.js
+++ src/utils/generateTasks.js
@@ -3,16 +3,20 @@ const generateDuration = () => 60 * 100 * Math.floor(Math.random() * 80 + 10)
 
 const generateTasks = () => {
   const count = 15 - Math.ceil(Math.random() * 100) % 5
-  let startTime = new Date(new Date().setHours(0, 0, 0, 0));
+  let startTime = new Date().setHours(0, 0, 0, 0).valueOf();
+  let endTime;
   return Array(count).fill(null).map((item, idx) => {
     startTime += generateDuration();
     const duration = generateDuration();
-    return {
+    endTime = startTime + duration;
+    const task = {
       name: `Auto-generated task #${idx}`,
-      startTime,
+      startTime: new Date(startTime),
       duration,
-      endTime: startTime + duration,
+      endTime: new Date(endTime),
     }
+    startTime = endTime + generateDuration();
+    return task
   })
 }
 
