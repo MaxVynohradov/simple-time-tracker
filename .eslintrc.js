48a5660d0d89aafe7f4a4376fd50dcaefd3adbf3
Maksym Vynohradov <mv@madappgang.com>
2020-04-14 23:09:27 +0300

    test(cypress): remove example files and configure eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,32 @@
 module.exports = {
-  extends: 'airbnb',
+  extends: [
+    'airbnb',
+    'plugin:cypress/recommended',
+    'plugin:chai-friendly/recommended',
+    'json-format',
+  ],
   parser: 'babel-eslint',
   rules: {
+    'semi': ['error'],
     'react/jsx-key': 2,
     'react/no-unused-prop-types': 2,
     'object-curly-spacing': ['error', 'always'],
-    'react/jsx-filename-extension': ['error', {'extensions': ['.jsx', '.js']}]
+    'react/jsx-filename-extension': ['error', { 'extensions': ['.jsx', '.js'] }],
+    'cypress/no-assigning-return-values': 'error',
+    'cypress/no-unnecessary-waiting': 'error',
+    'cypress/assertion-before-screenshot': 'warn',
+    'cypress/no-force': 'warn',
   },
   settings: {
     'import/resolver': {
-      node: {paths: ['./src'], extensions: ['.js']}
+      node: { paths: ['./src'], extensions: ['.js'] }
     },
   },
-  plugins: []
+  plugins: [
+    'cypress',
+    'chai-friendly',
+  ],
+  env: {
+    'cypress/globals': true
+  },
 };
