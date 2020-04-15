ca47b30e49b718729872fd8ad96711836de356fb
MaxVinogradov <vinogradov.max97@gmail.com>
2020-04-15 18:09:16 +0000

    Deploying to gh-pages from master e8d26e520638ef3b5455e0c5bfaba8490b93ccb0

---
+++ precache-manifest.3c01b6644ed9231e246b42f3d5804933.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "647e87e04616ef2f0aba2169ca38d615",
+    "url": "/simple-time-tracker/index.html"
+  },
+  {
+    "revision": "e4fd94fd61625b5e6114",
+    "url": "/simple-time-tracker/static/js/2.5b89c0a9.chunk.js"
+  },
+  {
+    "revision": "6d3c4316041b5905aa31b3ff080c57e2",
+    "url": "/simple-time-tracker/static/js/2.5b89c0a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e25a07ec862e6e056361",
+    "url": "/simple-time-tracker/static/js/main.5d341b19.chunk.js"
+  },
+  {
+    "revision": "ebf78d8229fb1ed904f5",
+    "url": "/simple-time-tracker/static/js/runtime-main.31c8a35f.js"
+  }
+]);
