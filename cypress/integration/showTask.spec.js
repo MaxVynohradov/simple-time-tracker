715474c2446bca8f0890ac2a544deaf1955b81de
Maksym Vynohradov <mv@madappgang.com>
2020-04-15 19:47:20 +0300

    test: add test on show page #2

--- cypress/integration/showTask.spec.js
+++ cypress/integration/showTask.spec.js
@@ -5,13 +5,9 @@
 import {
   getTableRows, getTaskNameInput, getTimerButton,
 } from '../pages/TasksPage';
-// import { getTaskStore, resetStore } from '../pages/ReduxStorePage';
-
-// import {
-//   TIMER_BUTTON, TIMER_CLOCK_FACE, TIMER_TEXT_INPUT, TASK_TABLE, REDUX_STORE,
-// } from '../constants/pageObjects';
 
 const TASK_NAME1 = 'Test task 1';
+const TASK_DURATION = 1;
 
 describe('Show Task Details', () => {
   before(() => {
@@ -19,6 +15,7 @@ describe('Show Task Details', () => {
     cy.visit(Cypress.env('BASE_URL'));
     getTaskNameInput().type(TASK_NAME1);
     getTimerButton().click();
+    cy.wait(TASK_DURATION * 1000);
     getTimerButton().click();
     getTableRows().get(':nth-child(6) > .MuiButtonBase-root').click();
   });
@@ -28,13 +25,39 @@ describe('Show Task Details', () => {
       cy.window().its('store')
         .then((store) => {
           const [{ id: taskId }] = store.getState().tasksStore.tasks;
-          cy.log('store.getState().tasksStore.tasks', taskId);
-          cy.url().should('have.include', `/tasks/${taskId}`);
+          cy.location().should((loc) => {
+            expect(loc.pathname).to.eq(`/simple-time-tracker/tasks/${taskId}`);
+          });
         });
+    });
+
+    it('has proper data on page', () => {
+      cy.window().its('store')
+        .then((store) => {
+          const [task] = store.getState().tasksStore.tasks;
+          cy.get('.MuiList-root > :nth-child(1)').should('have.text', `ID: ${task.id}`);
+          cy.get('.MuiList-root > :nth-child(3)').should('have.text', `Name: ${task.name}`);
+          cy.get('.MuiList-root > :nth-child(5)')
+            .should('have.text', `Start time: ${task.startTime.toTimeString().split(' ')[0]}`);
+          cy.get('.MuiList-root > :nth-child(7)')
+            .should('have.text', `End time: ${task.endTime.toTimeString().split(' ')[0]}`);
+          cy.get('.MuiList-root > :nth-child(9)').should('have.text', `Duration: 00:00:0${TASK_DURATION}`);
+        });
+    });
+
+    it('redirect correctly to main page', () => {
+      cy.get('.MuiButtonBase-root').click();
+      cy.location().should((loc) => {
+        expect(loc.pathname).to.eq('/simple-time-tracker/tasks');
+      });
     });
   });
 
-  // context('when pass incorrect task ID', () => {
-  //   // it('is redirected to /tasks/:taskId path', () => cy.url().should('include', '/tasks/'));
-  // });
+  context('when pass incorrect task ID', () => {
+    it('is redirected to /tasks/404 path', () => {
+      Cypress.on('uncaught:exception', () => false);
+      cy.visit(`${Cypress.env('BASE_URL')}/tasks/neverland`, { failOnStatusCode: false });
+      cy.url().should('include', '/simple-time-tracker/404');
+    });
+  });
 });
